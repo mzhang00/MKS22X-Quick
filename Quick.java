6ecfc1244d5c2e58e97e117c538f35973fada276
Michael Zhang <mzhang00@stuy.edu>
2019-03-19 21:34:29 -0400

    Test and update quicksort optimization

--- Quick.java
+++ Quick.java
@@ -163,7 +163,11 @@ public class Quick{
 
   public static void quicksort(int[] data){
     //quicksortH(data, 0, data.length - 1, 0, data.length - 1);
-    quicksortD(data, 0, data.length - 1, 0, data.length - 1);
+    if (data.length < 27){
+      insertionSort(data, 0, data.length);
+    }else{
+      quicksortD(data, 0, data.length - 1, 0, data.length - 1);
+    }
   }
 
   private static void quicksortH(int[] data, int start, int end, int newstart, int newend){
@@ -270,7 +274,7 @@ public class Quick{
 
 //  }
 
-/*  public static void main(String[]args){
+  public static void main(String[]args){
     System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
     int[]MAX_LIST = {1000000000,500,10};
     for(int MAX : MAX_LIST){
@@ -305,16 +309,15 @@ public class Quick{
       System.out.println();
     }
   }
-*/
 
-  public static void main(String[] args){
+/*  public static void main(String[] args){
     int[] ary = {10,1, 2, 40, 5, 10 , 20, 9, 1, 1, 1, 1};
     insertionSort(ary, 0, ary.length);
     for (int i : ary){
       System.out.print("" + i + " ");
     }
     System.out.println();
-  }
+  }*/
 
   private static void insertionSort(int[] ary, int lo, int hi){
     int temp;
@@ -329,4 +332,78 @@ public class Quick{
       ary[counter + 1] = temp;
     }
   }
+
+/*
+  //Sort testing code
+  private static final int INCREASE = 0;
+  private static final int DECREASE = 1;
+  private static final int STANDARD = 2;
+  private static final int SMALL_RANGE = 3;
+
+  private static String name(int i){
+    if(i==INCREASE)return "Increassing";
+    if(i==DECREASE)return "Decreassing";
+    if(i==STANDARD)return "Normal Random";
+    if(i==SMALL_RANGE)return "Random with Few Values";
+    return "Error categorizing array";
+
+  }
+
+  private static int create(int min, int max){
+    return min + (int)(Math.random()*(max-min));
+  }
+
+  private static int[]makeArray(int size,int type){
+    int[]ans =new int[size];
+    if(type == STANDARD){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-1000000,1000000);
+      }
+    }
+    else if(type == INCREASE){
+      int current = -5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current += 10;
+      }
+    }
+    else if(type == DECREASE){
+      int current = 5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current -= 10;
+      }
+    }
+    else if(type == SMALL_RANGE){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-5,5);
+      }
+    }
+    else{
+      ans = new int[0];//empty is default
+    }
+    return ans;
+  }
+
+  public static void main(String[]args){
+    if(args.length < 2)return;
+    
+    int size =  Integer.parseInt(args[0]);
+    int type =   Integer.parseInt(args[1]);
+
+    int [] start = makeArray(size,type);
+    int [] result = Arrays.copyOf(start,start.length);
+    Arrays.sort(result);
+    
+    long startTime = System.currentTimeMillis();
+    
+    quicksort(start);
+
+    long elapsedTime = System.currentTimeMillis() - startTime;
+    if(Arrays.equals(start,result)){
+      System.out.println("PASS Case "+name(type)+"\t array, size:"+start.length+"\t"+elapsedTime/1000.0+"sec ");
+    }else{
+      System.out.println("FAIL ! ERROR ! "+name(type)+" array, size:"+size+"  ERROR!");
+    }
+  }*/
 }
